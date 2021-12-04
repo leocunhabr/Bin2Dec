0394490aba5837b54698b0e59c8169d7da09e2e1
leocunhadev <leocunha.jobs@gmail.com>
2021-12-03 23:52:35 -0300

    Criação das funções gerais

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,2 +1,26 @@
-let teste = document.querySelector('#teste').innerText;
-console.log(teste);
+document.querySelector('#traduzir').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let $inputNumeroBinario = document.querySelector('#numeroBinario');
+    let $inputTraduzido = document.querySelector('#textoTraduzido');
+
+    iserirNumero(numeroSomado($inputNumeroBinario.value), $inputTraduzido);
+});
+
+function iserirNumero(numeroBinario, textoTraduzido) {
+    textoTraduzido.insertAdjacentHTML("beforebegin",
+        `<input type="text" class="form-control" id="textoTraduzido" value="${numeroBinario}" disabled>`);
+    textoTraduzido.remove();
+};
+
+
+function numeroSomado(inputNumeroBinario) {
+    let numeroSomado = 0;
+    let numeroBinarioSplit = inputNumeroBinario.split('');
+
+    numeroBinarioSplit.forEach((numero, indice) => {
+        numeroSomado += Math.pow(2, indice) * numeroBinarioSplit[numeroBinarioSplit.length - 1 - indice];
+    });
+
+    return numeroSomado;
+};
